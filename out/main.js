646860124c2fd6f23f6ffa7bc93ae004780beb21
dderpym <53957158+dderpym@users.noreply.github.com>
2022-05-21 20:45:29 +0000

    created a test object for serialization, found useful serialization library.

--- out/main.js
+++ out/main.js
@@ -1,9 +1,9 @@
 "use strict";
 exports.__esModule = true;
+var typescript_json_serializer_1 = require("typescript-json-serializer");
 var Game_1 = require("./base/Game");
 var Engine_1 = require("./rng/Engine");
-var Save_1 = require("./base/Save");
-var StepLog_1 = require("./base/StepLog");
+var Car_1 = require("./test/Car");
 function startGame() {
     Game_1.Game.getInstance().start();
 }
@@ -11,10 +11,8 @@ function testEngine(map) {
     var eng = new Engine_1.Random(map);
     console.log(eng.getRandom());
 }
-var save = new Save_1.Save();
-save.set("ivan", new StepLog_1.StepLog("a"));
-console.log(save.get("ivan"));
-var ivan = JSON.stringify(save);
-console.log(ivan);
-var parsedIvan = JSON.parse(ivan);
-parsedIvan.get("ivan").step();
+var car = new Car_1.Car("ivanmobile", new Car_1.Manufacturer("billy"));
+var serializer = new typescript_json_serializer_1.JsonSerializer();
+var string = JSON.stringify(serializer.serialize(car));
+var reserialized = serializer.deserializeObject(car, Car_1.Car);
+reserialized.honk();
